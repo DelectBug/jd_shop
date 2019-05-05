d1a930a685e405f54fadabc47b9196bf07c6b4a5
DelectBug <470114624@qq.com>
2019-05-05 21:43:25 +0800

    京东数据加公共底部

--- src/script/js/jd.js
+++ src/script/js/jd.js
@@ -1,4 +1,5 @@
-$('#nav_header').load("http://10.31.163.39/projectname/src/jd_nav_header.html");
-$('#header').load("http://10.31.163.39/projectname/src/jd_header.html");
-$('#main').load("http://10.31.163.39/projectname/src/jd_main.html");
-$('#main-floors').load("http://10.31.163.39/projectname/src/jd_main_floor.html");
+$('#nav_header').load("http://10.31.163.39/projectname/jd_shop/src/jd_nav_header.html");
+$('#header').load("http://10.31.163.39/projectname/jd_shop/src/jd_header.html");
+$('#main').load("http://10.31.163.39/projectname/jd_shop/src/jd_main.html");
+$('#main-floors').load("http://10.31.163.39/projectname/jd_shop/src/jd_main_floor.html");
+$('#footer').load("http://10.31.163.39/projectname/jd_shop/src/jd_footer.html");
