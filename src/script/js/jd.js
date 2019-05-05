6d5029c8181ff16a4352d23ffae0645a9e53894f
DelectBug <470114624@qq.com>
2019-05-05 16:48:46 +0800

    京东

---
+++ src/script/js/jd.js
@@ -0,0 +1,4 @@
+$('#nav_header').load("http://10.31.163.39/projectname/src/jd_nav_header.html");
+$('#header').load("http://10.31.163.39/projectname/src/jd_header.html");
+$('#main').load("http://10.31.163.39/projectname/src/jd_main.html");
+$('#main-floors').load("http://10.31.163.39/projectname/src/jd_main_floor.html");
