363bd50f4151ae8878774b4a43118f808b927dfe
DelectBug <470114624@qq.com>
2019-05-07 21:43:13 +0800

    5个静态页面完成

--- src/script/js/jd.js
+++ src/script/js/jd.js
@@ -3,3 +3,23 @@ $('#header').load("http://10.31.163.39/projectname/jd_shop/src/jd_header.html");
 $('#main').load("http://10.31.163.39/projectname/jd_shop/src/jd_main.html");
 $('#main-floors').load("http://10.31.163.39/projectname/jd_shop/src/jd_main_floor.html");
 $('#footer').load("http://10.31.163.39/projectname/jd_shop/src/jd_footer.html");
+$(document).ready(function() {
+    (function() {
+        console.log($(".navitems-group1").width())
+        console.log($(".a_m_inner_img").width())
+
+        class slideshow {
+            constructor() {
+                this.slider_wrapper = $("#main-floors")
+            }
+            init() {
+                console.log(this.slider_wrappe_width())
+            }
+            slider_wrappe_width() {
+                return $("#main-floors").width();
+            }
+
+        }
+        new slideshow().init();
+    })();
+})
