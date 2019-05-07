b1325228054e7bc999bcea6730a55d15156ac8c0
DelectBug <470114624@qq.com>
2019-05-07 11:34:31 +0800

    登入页面，购物车页面部分注册页面部分详情页页面

---
+++ src/script/js/jd_detail.js
@@ -0,0 +1,3 @@
+$('#nav_header').load("http://10.31.163.39/projectname/jd_shop/src/jd_nav_header.html");
+$('#header').load("http://10.31.163.39/projectname/jd_shop/src/jd_header.html");
+$('#footer').load("http://10.31.163.39/projectname/jd_shop/src/jd_footer.html");
